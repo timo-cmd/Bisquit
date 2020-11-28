90fafac1223aa2c0860fd3cf4d327b5d3e70d8ba
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-28 16:48:23 +0100

    Added type values for master node

--- include/utils.jl
+++ include/utils.jl
@@ -1,1 +1,20 @@
+using Dates
 
+struct Transaction
+  sender::String
+  recipient::String
+  amount::Int
+end
+
+struct Block
+  index::Int
+  timestamp::Dates.DateTime
+  transaction::Array{Transaction}
+  proof::Int
+  previous_hash::String
+end
+
+mutable struct blockchain 
+  chain::Array{Block}
+  current_transaction::Array{Transaction}
+end
