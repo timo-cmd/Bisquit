59d1046933de42f8e66430461e22e041115650fc
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-28 16:35:09 +0100

    Add base construction

--- sources/bisquit.jl
+++ sources/bisquit.jl
@@ -1,1 +1,13 @@
+module Bisquit
 
+# export all local vars from this module
+export Transaction, Blockchain, Block
+export new_transaction, new_block, create_genesis, hash, valid_proof, proof_of_work, last_block
+
+# include some utils here...
+using SHA # sha256bit for hashing
+using JSON # json for formatted output
+
+function new_transaction( )::int
+# add some values in here wip todo
+end
