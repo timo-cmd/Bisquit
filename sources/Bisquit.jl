2a8545adf3635ec785ebbc8df20ebf9f55944070
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-29 10:39:54 +0100

    added new transaction method

--- sources/Bisquit.jl
+++ sources/Bisquit.jl
@@ -11,6 +11,9 @@ include("../include/utils.jl")
 using SHA # sha256bit for hashing
 using JSON # json for formatted output
 
-function new_transaction( )::int
-# add some values in here wip todo
+function new_transaction( blockchain::Blockchain, sender::String, recipient::String, amount::Int, )::int
+  # add some values in here wip todo
+  transaction = Transaction( sender, recipient, amount )
+  push!( blockchain.current_transactions, transaction )
+  return blockchain.chain[ end ].index + 1
 end
