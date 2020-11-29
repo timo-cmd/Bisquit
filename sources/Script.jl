dde77012ca418c3c8777e2124d0bef61e3c62896
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-11-29 15:59:33 +0100

    script to install all julia deps

---
+++ sources/Script.jl
@@ -0,0 +1,8 @@
+# script to install all deps
+import Pkg; Pkg.add("JSON")
+import Pkg; Pkg.add("SHA")
+import Pkg; Pkg.add("Dates")
+import Bisquit; Pkg.add("Bisquit")
+import Genie, Genie.Router, Genie.Renderer.Json, Genie.Requests; Pkg.add("Genie")
+import HTTP; Pkg.add("HTTP")
+import UUIDs; Pkg.add("UUIDs")
